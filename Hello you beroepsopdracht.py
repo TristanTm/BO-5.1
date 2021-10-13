4b23ffd77c1b422bcf9d43c92b0566ccd2790b5c
TristanTm <33825@ma-web.nl>
2021-10-13 09:12:18 +0200

    Eerste commit

---
+++ Hello you beroepsopdracht.py
@@ -0,0 +1,8 @@
+def vraag1(): 
+    vraag1 = input("De taliban heeft de stad ingenomen, wat moeten we doen? A) We vertoppen ons tijdelijk. B) We vechten terug met wat we hebben!. C) We proberen zo snel mogelijk te vluchten!.  : ")
+    if vraag1 == "A":
+        print("oke doen we ")
+    elif vraag1 == "B":
+        print("Oke ook erg goed ")
+    elif vraag1 == "C":
+        print("Toppie ")
